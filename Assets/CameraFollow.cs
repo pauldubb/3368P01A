97901c4f23fa584a660f563a5754b10a30e6e38e
Paul Dubberley <pauldubberley@hotmail.com>
2020-08-26 19:18:19 -0500

    Project Initialization

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _objectToFollow = null;
+
+    Vector3 _objectOffset;
+
+    private void Awake()
+    {
+        _objectOffset = this.transform.position - _objectToFollow.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = _objectToFollow.position + _objectOffset;
+    }
+}
---
+++ Assets/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 12f;
+    [SerializeField] float _turnSpeed = 3f;
+    Rigidbody _rb = null;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveShip();
+        TurnShip();
+    }
+
+    void MoveShip()
+    {
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * _moveSpeed;
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+        _rb.AddForce(moveDirection);
+    }
+
+    void TurnShip()
+    {
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * _turnSpeed;
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+        _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+}
